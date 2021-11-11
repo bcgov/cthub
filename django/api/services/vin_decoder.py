a1fd3542de08018ad505e1a2eb2b1be227e1e822
NavpreetGrewal <ngrewal002@gmail.com>
2021-11-10 16:06:54 -0800

    VIN Decoder Function

---
+++ django/api/services/vin_decoder.py
@@ -0,0 +1,31 @@
+import logging
+import requests
+import json
+
+from api.models.icbc_registration_data import IcbcRegistrationData
+from rest_framework.response import Response
+from django.core.paginator import Paginator
+
+def decoder():
+    url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/'
+    
+    decoded_result = [1,2]
+    vin_queryset = IcbcRegistrationData.objects.values_list('vin', flat=True).order_by('vin')
+    pages = Paginator(vin_queryset, 50)
+    for p in pages:
+        page = pages.page(p.number)
+        vin_batch = ';'.join( page.object_list )
+        post_fields = {'format': 'json', 'data': vin_batch}
+        try:
+            response = requests.post(url, data=post_fields)
+            if not response.status_code == 200:
+                LOGGER.error("Error: Decoding Failed! %s", response)
+                return
+            json_response = response.json()
+            decoded_result.append(json_response)
+        except requests.exceptions.RequestException as e:
+            LOGGER.error("Error: {}".format(e))
+            return
+
+    return decoded_result
+
