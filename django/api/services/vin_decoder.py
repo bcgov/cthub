d6fbf0e3900488d16b1cc5ffc71c00179f416cfe
NavpreetGrewal <ngrewal002@gmail.com>
2021-11-12 09:32:30 -0800

    Created command file to run decoder function

--- django/api/services/vin_decoder.py
+++ django/api/services/vin_decoder.py
@@ -9,23 +9,23 @@ from django.core.paginator import Paginator
 def decoder():
     url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/'
     
-    decoded_result = [1,2]
+    json_response = None
     vin_queryset = IcbcRegistrationData.objects.values_list('vin', flat=True).order_by('vin')
     pages = Paginator(vin_queryset, 50)
-    for p in pages:
-        page = pages.page(p.number)
-        vin_batch = ';'.join( page.object_list )
-        post_fields = {'format': 'json', 'data': vin_batch}
-        try:
-            response = requests.post(url, data=post_fields)
-            if not response.status_code == 200:
-                LOGGER.error("Error: Decoding Failed! %s", response)
-                return
-            json_response = response.json()
-            decoded_result.append(json_response)
-        except requests.exceptions.RequestException as e:
-            LOGGER.error("Error: {}".format(e))
+    #for p in pages:
+    page = pages.page(1)
+    vin_batch = ';'.join( page.object_list )
+    post_fields = {'format': 'json', 'data': vin_batch}
+    try:
+        response = requests.post(url, data=post_fields)
+        if not response.status_code == 200:
+            LOGGER.error("Error: Decoding Failed! %s", response)
             return
+        json_response = response.json()
+        # decoded_result.append(json_response)
+    except requests.exceptions.RequestException as e:
+        LOGGER.error("Error: {}".format(e))
+        return
 
-    return decoded_result
+    return json_response
 
