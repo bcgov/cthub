4b97bd73aee42be2725882d141c7fb95155cc140
Kuan Fan <kuan.fan@gov.bc.ca>
2021-11-23 17:07:11 -0800

    s

--- openshift/templates/patroni-2.1.1/contrib/root/usr/share/scripts/patroni/post_init.sh
+++ openshift/templates/patroni-2.1.1/contrib/root/usr/share/scripts/patroni/post_init.sh
@@ -7,10 +7,13 @@ if [[ (! -z "$APP_USER") &&  (! -z "$APP_PASSWORD") && (! -z "$APP_DATABASE")]];
 
   echo "Creating database ${APP_DATABASE}"
   psql "$1" -w -c "CREATE DATABASE \"${APP_DATABASE}\" OWNER \"${APP_USER}\" ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
-
-  echo "Creating extensions" 
+  echo "Creating database ${METABASE_DATABASE}"
+  psql "$1" -w -c "CREATE DATABASE \"${METABASE_DATABASE}\" OWNER \"${METABASE_USER}\" ENCODING '${METABASE_DB_ENCODING:-UTF8}' LC_COLLATE = '${METABASE_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${METABASE_DB_LC_CTYPE:-en_US.UTF-8}'"
+  
+  echo "Creating extension hstore for ${APP_DATABASE}" 
   psql -U postgres -q -d "${APP_DATABASE}" -c 'create extension if not exists hstore'
-  psql -U postgres -q -d "${APP_DATABASE}" -c 'create extension if not exists citext'
+  echo "Creating extension citext for ${METABASE_DATABASE}" 
+  psql -U postgres -q -d "${METABASE_DATABASE}" -c 'create extension if not exists citext'
 
 else
   echo "Skipping user creation"
