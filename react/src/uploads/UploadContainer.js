c3f115725ee336097e67247f54a69d3ee29326f7
Richard Tan <richard.tan@gov.bc.ca>
2021-11-30 13:55:52 -0800

    Further cleanup

--- react/src/uploads/UploadContainer.js
+++ react/src/uploads/UploadContainer.js
@@ -2,14 +2,14 @@ import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 import CircularProgress from '@mui/material/CircularProgress';
 import React, { useState, useEffect } from 'react';
-import ROUTES_UPLOAD from './routes'
+import ROUTES_UPLOAD from './routes';
 import UploadPage from './components/UploadPage';
 
 const UploadContainer = () => {
   const [uploadFiles, setUploadFiles] = useState([]); // array of objects for files to be uploaded
-  const [datasetList, setDatasetList] = useState(['LDV rebates']); //holds the array of names of datasets
+  const [datasetList, setDatasetList] = useState(['LDV rebates']); // holds the array of names of datasets
   const [loading, setLoading] = useState(false);
-  const [datasetSelected, setDatasetSelected] = useState(''); //string identifying which dataset is being uploaded
+  const [datasetSelected, setDatasetSelected] = useState(''); // string identifying which dataset is being uploaded
   const refreshList = () => {
     // setLoading(true);
     // axios.get(ROUTES.LIST).then((response) => {
