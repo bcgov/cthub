cf0ed7c71d63dddf9919169734d2a7f07eb2294e
Jamie Popkin <popkinj@littleearth.ca>
2021-12-06 05:52:18 -0800

    Charger model

---
+++ django/api/models/charger_rebates.py
@@ -0,0 +1,88 @@
+from auditable.models import Auditable
+from django.db import models
+
+
+class ChargerRebates(Auditable):
+
+    organization = models.CharField(
+        blank=True,
+        null=True,
+        max_length=250,
+        unique=False
+    )
+
+    region = models.CharField(
+        blank=True,
+        null=True,
+        max_length=200,
+        unique=False
+    )
+
+    city = models.CharField(
+        blank=True,
+        null=True,
+        max_length=200,
+        unique=False
+    )
+
+    address = models.CharField(
+        blank=True,
+        null=True,
+        max_length=200,
+        unique=False
+    )
+
+    postal_code = models.CharField(
+        blank=True,
+        null=True,
+        max_length=200,
+        unique=False
+    )
+
+    postal_code = models.CharField(
+        blank=True,
+        null=True,
+        max_length=200,
+        unique=False
+    )
+
+    number_of_fast_charging_stations = models.IntegerField(
+        blank=True,
+        null=True
+    )
+
+    in_service_date = models.CharField(
+        blank=True,
+        null=True,
+        max_length=100,
+        unique=False
+    )
+
+    expected_in_service_date = models.DateField(
+        blank=True,
+        null=True
+    )
+
+    announced = models.CharField(
+        blank=True,
+        null=True,
+        max_length=200,
+        unique=False
+    )
+
+    rebate_paid = models.DecimalField(
+        blank=True,
+        null=True,
+        max_digits=20,
+        decimal_places=2
+    )
+
+    notes = models.CharField(
+        blank=True,
+        null=True,
+        max_length=250,
+        unique=False
+    )
+
+    class Meta:
+        db_table = "charger_rebates"
