94f3ceec27eb056c2c61197992726d9ce678a93e
emi-hi <emily.hillier@gmail.com>
2021-12-10 12:38:31 -0800

    -switches redirect to usehistory

--- react/src/dashboard/DashboardContainer.js
+++ react/src/dashboard/DashboardContainer.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { withRouter, Redirect } from 'react-router-dom';
+import { withRouter, useHistory } from 'react-router-dom';
 
-const DashboardContainer = () => (
-  <div className="row">
-    <div className="col-12">
-      <Redirect to="/upload" />
+const DashboardContainer = () => {
+  const { location } = window;
+  const { pathname } = location;
+  const history = useHistory();
+  return (
+
+    <div className="row">
+      <div className="col-12">
+        {pathname === '/' && history.push('/upload')}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default withRouter(DashboardContainer);
