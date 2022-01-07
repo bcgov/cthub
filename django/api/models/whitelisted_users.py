0846939b55e37596c2d7224bc3a6a3345a309f42
emi-hi <emily.hillier@gmail.com>
2022-01-07 13:51:03 -0800

    -adds model for whitelisted users

---
+++ django/api/models/whitelisted_users.py
@@ -0,0 +1,14 @@
+from django.db import models
+from auditable.models import Auditable
+
+
+class WhitelistedUsers(Auditable):
+    user = models.CharField(
+        blank=False,
+        null=False,
+        unique=True,
+        max_length=100,
+    )
+
+    class Meta:
+        db_table = 'whitelisted_users'
