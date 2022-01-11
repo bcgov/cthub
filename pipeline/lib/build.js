70a66489f80275c6e3d2adc3a17c0c81f165845f
Kuan Fan <kuan.fan@gov.bc.ca>
2022-01-11 13:09:52 -0800

    update pipeline folder

--- pipeline/lib/build.js
+++ pipeline/lib/build.js
@@ -13,7 +13,7 @@ module.exports = settings => {
   // The building of your cool app goes here ▼▼▼
 
   // build frontend
-  /*
+  
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-bc.yaml`, {
     'param':{
       'NAME': phases[phase].name,
@@ -22,7 +22,7 @@ module.exports = settings => {
       'GIT_URL': oc.git.http_url,
       'GIT_REF': oc.git.ref
     }
-  }))*/
+  }))
 
   //build backend
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/backend/backend-bc.yaml`, {
@@ -33,7 +33,7 @@ module.exports = settings => {
       'GIT_URL': oc.git.http_url,
       'GIT_REF': oc.git.ref
     }
-  }))
+  }))*/
 
   oc.applyRecommendedLabels(
     objects,
