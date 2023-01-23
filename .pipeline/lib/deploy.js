976b55f09cf91e3b577ce4c65f39d5890eedc2de
kuanfandevops <kuan.fan@gov.bc.ca>
2023-01-23 15:56:21 -0800

    remove dev pr based

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -23,36 +23,6 @@ module.exports = settings => {
 
   // The deployment of your cool app goes here ▼▼▼
 
-  //create network security policies for internal pod to pod communications
-  if(phase === 'dev') {
-
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/knp/knp-env-pr.yaml`, {
-      'param': {
-        'SUFFIX': phases[phase].suffix,
-        'ENVIRONMENT': phases[phase].phase
-      }
-    }))
-
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/patroni-2.1.1/templates/prerequisite.yaml`, {
-      'param': {
-        'SUFFIX': phases[phase].suffix
-      }
-    }))
-
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/patroni-2.1.1/templates/deploy.yaml`, {
-      'param': {
-        'SUFFIX': phases[phase].suffix,
-        'CPU_REQUEST': phases[phase].patroniCpuRequest,
-        'CPU_LIMIT': phases[phase].patroniCpuLimit,
-        'MEMORY_REQUEST': phases[phase].patroniMemoryRequest,
-        'MEMORY_LIMIT': phases[phase].patroniMemoryLimit,
-        'REPLICAS': phases[phase].patroniReplica,
-        'PVC_SIZE': phases[phase].patroniPvcSize,
-        'STORAGE_CLASS': phases[phase].storageClass
-      }
-    }))
-  }
-
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-dc.yaml`, {
     'param': {
       'NAME': phases[phase].name,
