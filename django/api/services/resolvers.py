c6816e0c3788f6d93b1c1bb0fab7f3322773556d
tim738745 <tim.chen@gov.bc.ca>
2024-07-11 11:39:36 -0700

    fix: 366 - (vin decoding jobs)/resolver fixes/changes

--- django/api/services/resolvers.py
+++ django/api/services/resolvers.py
@@ -1,7 +1,8 @@
 from dns.resolver import Resolver
+from email_validator import caching_resolver
 
 
 def get_google_resolver():
     resolver = Resolver()
     resolver.nameservers = ["8.8.8.8"]
-    return resolver
+    return caching_resolver(dns_resolver=resolver)
