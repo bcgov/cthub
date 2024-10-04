56c61969226ba58ea5fde4589c9fcdd7b320bd02
julianforeman <julianforeman@me.com>
2024-10-04 10:40:44 -0700

    Removing id insertion and adding unique constraint to name

--- django/api/migrations/0038_addregions.py
+++ django/api/migrations/0038_addregions.py
@@ -1,4 +1,4 @@
-from django.db import migrations
+from django.db import migrations, models
 from datetime import datetime
 
 def add_region_data(apps, schema_editor):
@@ -7,20 +7,19 @@ def add_region_data(apps, schema_editor):
     current_timestamp = datetime.now()
 
     regions_data = [
-        {"id": 1, "name": "Nechako"},
-        {"id": 2, "name": "Northeast"},
-        {"id": 3, "name": "Cariboo"},
-        {"id": 4, "name": "North Coast"},
-        {"id": 5, "name": "Vancouver Island/Coast"},
-        {"id": 6, "name": "Mainland/Southwest"},
-        {"id": 7, "name": "Thompson/Okanagan"},
-        {"id": 8, "name": "Kootenay"},
-        {"id": 9, "name": "Across BC"},
+        {"name": "Nechako"},
+        {"name": "Northeast"},
+        {"name": "Cariboo"},
+        {"name": "North Coast"},
+        {"name": "Vancouver Island/Coast"},
+        {"name": "Mainland/Southwest"},
+        {"name": "Thompson/Okanagan"},
+        {"name": "Kootenay"},
+        {"name": "Across BC"},
     ]
     
     for region in regions_data:
         Regions.objects.get_or_create(
-            id=region["id"],
             defaults={
                 "create_timestamp": current_timestamp,
                 "create_user": "SYSTEM",
@@ -37,5 +36,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.AlterField(
+            model_name='regions',
+            name='name',
+            field=models.CharField(max_length=250, null=False, unique=True)
+        ),
         migrations.RunPython(add_region_data),
     ]
