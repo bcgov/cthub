a1a77d2f7aa8ccc573d5c158a08693792a0ce529
tim738745 <98717409+tim738745@users.noreply.github.com>
2026-07-19 21:19:26 -0700

    remove temp stuff (#525)

--- django/api/viewsets/icbc.py
+++ django/api/viewsets/icbc.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import GenericViewSet
 from api.token_authentication import CustomTokenAuthentication
 from api.models.icbc import IcbcRecord
-from api.services.icbc import get_icbc_ev_records, temp1, temp2, temp3
+from api.services.icbc import get_icbc_ev_records
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -15,16 +15,4 @@ class IcbcRecordViewset(GenericViewSet):
     def get_icbc_ev_records(self, request):
         vins = request.data.get("vins")
         icbc_records = get_icbc_ev_records(vins)
-        return Response(icbc_records)
-
-    @action(detail=False, methods=["post"])
-    def temp(self, request):
-        vins = request.data.get("vins")
-        selection = request.data.get("selection")
-        if selection == "temp1":
-            icbc_records = temp1(vins)
-        elif selection == "temp2":
-            icbc_records = temp2(vins)
-        elif selection == "temp3":
-            icbc_records = temp3(vins)
         return Response(icbc_records)
